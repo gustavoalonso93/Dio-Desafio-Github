800ecd079fa0de561901c0d9ff001cd08ba77007
Gustavo <gustavoalonso93@hotmail.com>
2022-06-23 09:52:28 -0300

    FuOperação com array

--- Collections/collection-01/src/main/kotlin/one/digitalinnovation/collection/testelist.kt
+++ Collections/collection-01/src/main/kotlin/one/digitalinnovation/collection/testelist.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collection
 
 fun main(){
-    val joao  = Funcionario(nome = "joao", salario = 1000.0 )
-    val pedro = Funcionario(nome = "pedro", salario = 3500.0 )
-    val maria = Funcionario(nome = "maria", salario = 2000.0)
+    val joao  = Funcionario(nome = "joao", salario = 1000.0 , tipo = "clt")
+    val pedro = Funcionario(nome = "pedro", salario = 3500.0 , tipo = "pj")
+    val maria = Funcionario(nome = "maria", salario = 2000.0, tipo = "clt")
 
     val funcionarios = listOf(joao,pedro,maria)
 
@@ -14,11 +14,17 @@ fun main(){
     println("---------------")
     println(funcionarios.find { it.nome == "maria" })
 
+    println("----tipo----------")
+    funcionarios
+        .groupBy { it.tipo }
+        .forEach{println(it)}
+
 
 }
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipo: String
 ){
     override fun toString(): String =
         """
